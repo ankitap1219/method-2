aefe3ab764bd6f15357bf6bd216dd110a0e11653
root <root@ip-172-31-80-125.ec2.internal>
2024-04-13 00:28:06 +0000

    fix

--- bash.sh
+++ bash.sh
@@ -1,1 +1,6 @@
-
+#!/bin/bash
+sudo yum -y install git
+sudo git clone https://github.com/ankitap1219/flight-prediction.git
+cd flight-prediction/
+pip3 install -r requirements.txt
+screen -m -d python3 app.py
